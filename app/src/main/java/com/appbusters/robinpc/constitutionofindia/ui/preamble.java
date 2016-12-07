535ed32573c57d0d23c13b8b5069b8221c34e014
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-08 00:02:49 +0530

    applying the same layout throughout

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/preamble.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/preamble.java
@@ -17,14 +17,5 @@ public class preamble extends AppCompatActivity {
         setContentView(R.layout.activity_preamble);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Jai Hind!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
