9da6ab1e8076d647bb177c4cb14ea5fcf7133e26
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-14 00:02:48 +0530

    part 2 done

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/all_parts/Part3.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/all_parts/Part3.java
@@ -1,9 +1,9 @@
 package com.appbusters.robinpc.constitutionofindia.ui.all_parts;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -200,16 +200,169 @@ public class Part3 extends AppCompatActivity {
         builder.setMessage(this.getString(R.string.article35)).create();
         builder.show();
     }
-
-
-
     public void article21A(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(this.getString(R.string.article21A)).create();
         builder.show();
     }
 
+    public void onClick(View v){
 
+        AlertDialog.Builder alert= new AlertDialog.Builder(this);
+        String[] array={getString(R.string.article12)};
 
+        switch (v.getId()){
+            case R.id.three12:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three13:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three14:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three15:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three16:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three17:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three18:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three19:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three20:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three21:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three21A:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three22:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three23:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three24:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three25:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three26:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three27:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three28:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three29:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three30:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three31:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three31A:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three31B:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three31C:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three31D:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three32:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three32A:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three33:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three34:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+            case R.id.three35:
+            {
+                alert.setMessage(array[0]).setIcon(R.drawable.dialog).setTitle("Article 12").create().show();
+                break;
+            }
+        }
+    }
 
 }
