89aff1d27113565ac7571785a646b01aa1ad1115
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-18 20:23:16 +0530

    PART 3 JAVA COMPLETE

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/all_parts/Part3.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/all_parts/Part3.java
@@ -28,188 +28,25 @@ public class Part3 extends AppCompatActivity {
             }
         });
     }
-    public void article12(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article12)).create();
-        builder.show();
-    }
-    public void article13(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article13)).create();
-        builder.show();
-    }
-    public void article14(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article14)).create();
-        builder.show();
-    }
-    public void article15(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article15)).create();
-        builder.show();
-    }
-    public void article16(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article16)).create();
-        builder.show();
-    }
-    public void article17(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article17)).create();
-        builder.show();
-    }
-    public void article18(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article18)).create();
-        builder.show();
-    }
-    public void article19(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article19)).create();
-        builder.show();
-    }
-    public void article20(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article20)).create();
-        builder.show();
-    }
-    public void article21(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article21)).create();
-        builder.show();
-    }
-    public void article22(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article22)).create();
-        builder.show();
-    }
-    public void article23(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article23)).create();
-        builder.show();
-    }
-    public void article24(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article24)).create();
-        builder.show();
-    }
-    public void article25(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article25)).create();
-        builder.show();
-    }
-    public void article26(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article26)).create();
-        builder.show();
-    }
-    public void article27(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article27)).create();
-        builder.show();
-    }
-    public void article28(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article28)).create();
-        builder.show();
-    }
-    public void article29(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article29)).create();
-        builder.show();
-    }
-    public void article30(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article30)).create();
-        builder.show();
-    }
-    public void article31(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article31)).create();
-        builder.show();
-    }
-    public void article31A(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article31A)).create();
-        builder.show();
-    }
-    public void article31B(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article31B)).create();
-        builder.show();
-    }
-    public void article31C(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article31C)).create();
-        builder.show();
-    }
-    public void article31D(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article31D)).create();
-        builder.show();
-    }
-    public void article32(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article32)).create();
-        builder.show();
-    } public void article32A(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article32A)).create();
-        builder.show();
-    } public void article33(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article33)).create();
-        builder.show();
-    }
-    public void article34(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article34)).create();
-        builder.show();
-    }
-    public void article35(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article35)).create();
-        builder.show();
-    }
-    public void article21A(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article21A)).create();
-        builder.show();
-    }
 
     public void onClick(View v){
 
         AlertDialog.Builder alert= new AlertDialog.Builder(this);
-        String[] array={getString(R.string.article12)};
+        String[] array={getString(R.string.article12),getString(R.string.article13),
+                getString(R.string.article14),getString(R.string.article15),
+                getString(R.string.article16),getString(R.string.article17),
+                getString(R.string.article18),getString(R.string.article19),
+                getString(R.string.article20),getString(R.string.article21),
+                getString(R.string.article21A),getString(R.string.article22),
+                getString(R.string.article23),getString(R.string.article24),
+                getString(R.string.article25),getString(R.string.article26),
+                getString(R.string.article27),getString(R.string.article28),
+                getString(R.string.article29),getString(R.string.article30),
+                getString(R.string.article31),getString(R.string.article31A),
+                getString(R.string.article31B),getString(R.string.article31C),
+                getString(R.string.article31D),getString(R.string.article32),
+                getString(R.string.article32A),getString(R.string.article33),
+                getString(R.string.article34),getString(R.string.article35)};
 
         switch (v.getId()){
             case R.id.three12:
