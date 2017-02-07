4348cbf83b68f26b714e4fd4f6b37d96e1942f6a
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-07 20:35:45 +0530

    Data for Adapter

---
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/model/Data.java
@@ -0,0 +1,32 @@
+package com.appbusters.robinpc.constitutionofindia.model;
+
+/**
+ * Created by robinkamboj on 07/02/17.
+ */
+
+public class Data {
+
+    public String heading;
+    public String description;
+
+    public Data(String heading, String description) {
+        this.heading = heading;
+        this.description = description;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
