a6bf08e07295c917dd986ff0f958c2a8b4c3f352
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-08 14:42:34 +0530

    ...

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/ScrollingActivity.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/ScrollingActivity.java
@@ -21,9 +21,6 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
