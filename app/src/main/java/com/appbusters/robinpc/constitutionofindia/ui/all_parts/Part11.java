45dd4f858551d82bdf7128abafc46febc9e543a4
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-09 21:15:18 +0530

    part 9A

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/all_parts/Part11.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/all_parts/Part11.java
@@ -1,129 +1,82 @@
 package com.appbusters.robinpc.constitutionofindia.ui.all_parts;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.appbusters.robinpc.constitutionofindia.R;
+import com.appbusters.robinpc.constitutionofindia.controller.Recycler_View_Adapter;
+import com.appbusters.robinpc.constitutionofindia.model.Data;
+import com.appbusters.robinpc.constitutionofindia.ui.ABOUT;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Part11 extends AppCompatActivity {
+
+    String[] headers, desc;
+    RecyclerView recyclerView;
+    List<Data> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part11);
 
+        headers = getResources().getStringArray(R.array.part_9A);
+        desc = new String[]{getString(R.string.article243P),getString(R.string.article243Q),getString(R.string.article243R),
+                getString(R.string.article243S),getString(R.string.article243T),getString(R.string.article243U),
+                getString(R.string.article243V),getString(R.string.article243W),getString(R.string.article243X),
+                getString(R.string.article243Y),getString(R.string.article243Z),getString(R.string.article243ZA),
+                getString(R.string.article243ZB),getString(R.string.article243ZC),getString(R.string.article243ZD),
+                getString(R.string.article243ZE),getString(R.string.article243ZF),getString(R.string.article243ZG)};
+
+        data = fillWithData();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        Recycler_View_Adapter adapter = new Recycler_View_Adapter(data, getApplicationContext());
+        recyclerView.setAdapter(adapter);
     }
-    public void article243P(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
+
+    private List<Data> fillWithData(){
+        List<Data> data = new ArrayList<>();
+        for(int i = 1; i<=headers.length; i++){
+            data.add(new Data(" ", headers[i-1], desc[i-1]));
+        }
+        return data;
     }
-    public void article243Q(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.menu_scrolling, menu);
+        return true;
     }
-    public void article243R(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id ==  R.id.aboutt)
+        {
+            Intent i=new Intent(this,ABOUT.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-    public void article243S(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243T(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243U(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243V(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243W(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243X(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243Y(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243Z(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243A)).create();
-        builder.show();
-    }
-    public void article243ZA(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZA)).create();
-        builder.show();
-    }
-    public void article243ZB(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZB)).create();
-        builder.show();
-    }
-    public void article243ZC(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZC)).create();
-        builder.show();
-    }
-    public void article243ZD(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZD)).create();
-        builder.show();
-    }
-    public void article243ZE(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZE)).create();
-        builder.show();
-    }
-    public void article243ZF(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZF)).create();
-        builder.show();
-    }
-    public void article243ZG(View view)
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(this.getString(R.string.article243ZG)).create();
-        builder.show();
-    }
+
 }
