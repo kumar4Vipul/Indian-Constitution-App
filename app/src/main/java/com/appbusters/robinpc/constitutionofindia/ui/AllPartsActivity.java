c63f1632409c0d3ca88a5d605704176aac22dfba
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-28 22:05:41 +0530

    All Parts Activity.

---
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/AllPartsActivity.java
@@ -0,0 +1,15 @@
+package com.appbusters.robinpc.constitutionofindia.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinpc.constitutionofindia.R;
+
+public class AllPartsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_parts);
+    }
+}
