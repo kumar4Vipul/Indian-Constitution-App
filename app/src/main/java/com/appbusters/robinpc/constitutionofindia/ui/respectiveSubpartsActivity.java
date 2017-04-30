8433eadeb1c6f86ccb6c7318fe9439a197ed73f1
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-30 16:18:08 +0530

    en route.

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/respectiveSubpartsActivity.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/respectiveSubpartsActivity.java
@@ -3,10 +3,14 @@ package com.appbusters.robinpc.constitutionofindia.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.appbusters.robinpc.constitutionofindia.R;
 
 public class respectiveSubpartsActivity extends AppCompatActivity {
+
+    private String category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,9 @@ public class respectiveSubpartsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        category = getIntent().getStringExtra("subTitle");
+        textView.setText(category);
     }
 }
