fa9b146bb4808002a68669b9dbd1404081b1cfe4
Robin Kamboj <dev.robinkamboj@gmail.com>
2019-05-24 19:34:30 +0530

    bookmarks fragment basic layout

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/home/fragments/bookmarks_fragment/BookmarkFragment.kt
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/home/fragments/bookmarks_fragment/BookmarkFragment.kt
@@ -1,27 +1,19 @@
 package com.appbusters.robinpc.constitutionofindia.ui.home.fragments.bookmarks_fragment
 
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.appbusters.robinpc.constitutionofindia.R
+import com.appbusters.robinpc.constitutionofindia.ui.base.BaseFragment
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class BookmarkFragment : Fragment() {
+class BookmarkFragment : BaseFragment() {
 
     companion object {
         fun newInstance() = BookmarkFragment()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_bookmark
+    }
+
+    override fun setup() {
+
     }
 }
