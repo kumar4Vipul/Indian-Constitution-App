a1c05ed4143d51adc061a6ac27dce2fddd45ce57
Robin Kamboj <dev.robinkamboj@gmail.com>
2019-05-24 22:12:32 +0530

    bookmark categories layout

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/home/fragments/bookmarks_fragment/BookmarkFragment.kt
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/home/fragments/bookmarks_fragment/BookmarkFragment.kt
@@ -2,8 +2,14 @@ package com.appbusters.robinpc.constitutionofindia.ui.home.fragments.bookmarks_f
 
 import com.appbusters.robinpc.constitutionofindia.R
 import com.appbusters.robinpc.constitutionofindia.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_bookmark.*
 
 class BookmarkFragment : BaseFragment() {
+
+    private var savedSchedulesCount = 0
+    private var savedAmendmentsCount = 0
+    private var savedArticlesCount = 0
+    private var savedPreambleCount = 0
 
     companion object {
         fun newInstance() = BookmarkFragment()
@@ -14,6 +20,37 @@ class BookmarkFragment : BaseFragment() {
     }
 
     override fun setup() {
+        setClickListeners()
+        fetchSavedItemCount()
+    }
 
+    private fun setClickListeners() {
+        savedSchedulesButton.setOnClickListener {
+        }
+        savedArticlesButton.setOnClickListener {
+        }
+        savedAmendmentsButton.setOnClickListener {
+        }
+        savedPreambleButton.setOnClickListener {
+        }
+    }
+
+    private fun fetchSavedItemCount() {
+        //TODO: fetch saved item count for each category by room and store them in count variables
+        setCountForFetchResult()
+    }
+
+    private fun setCountForFetchResult() {
+        context?.let {
+            savedSchedulesCountTv.text = getCountString(savedSchedulesCount)
+            savedArticlesCountTv.text = getCountString(savedArticlesCount)
+            savedAmendmentsCountTv.text = getCountString(savedAmendmentsCount)
+            savedPreambleCountTv.text = getCountString(savedPreambleCount)
+        }
+    }
+
+    private fun getCountString(count: Int): String {
+        val savedString = getString(R.string.saved)
+        return count.toString().plus(" ").plus(savedString)
     }
 }
