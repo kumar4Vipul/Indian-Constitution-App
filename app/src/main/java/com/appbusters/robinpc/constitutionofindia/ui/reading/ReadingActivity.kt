5d9db789d39e764ec779a5e9aea95c6bc5589770
Robin Kamboj <dev.robinkamboj@gmail.com>
2019-05-25 15:40:34 +0530

    bookmark, rate and share view in fixed bottom view in read activity

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/reading/ReadingActivity.kt
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/ui/reading/ReadingActivity.kt
@@ -2,7 +2,9 @@ package com.appbusters.robinpc.constitutionofindia.ui.reading
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.text.Html
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.appbusters.robinpc.constitutionofindia.ConstitutionApp
 import com.appbusters.robinpc.constitutionofindia.R
@@ -11,6 +13,10 @@ import com.appbusters.robinpc.constitutionofindia.data.model.ReadElement
 import com.appbusters.robinpc.constitutionofindia.di.component.activity.DaggerReadActivityComponent
 import com.appbusters.robinpc.constitutionofindia.ui.base.BaseActivity
 import com.appbusters.robinpc.constitutionofindia.ui.reading.adapter.DummyTagListAdapter
+import com.appbusters.robinpc.constitutionofindia.utils.Constants.Companion.CATEGORY_AMENDMENTS
+import com.appbusters.robinpc.constitutionofindia.utils.Constants.Companion.CATEGORY_PARTS
+import com.appbusters.robinpc.constitutionofindia.utils.Constants.Companion.CATEGORY_PREAMBLE
+import com.appbusters.robinpc.constitutionofindia.utils.Constants.Companion.CATEGORY_SCHEDULES
 import com.appbusters.robinpc.constitutionofindia.utils.Constants.Companion.EXTRA_READ_ELEMENT
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_reading.*
@@ -24,7 +30,10 @@ class ReadingActivity : BaseActivity() {
     @Inject
     lateinit var dummyTagsAdapter: DummyTagListAdapter
 
-    private var readElement: ReadElement? = null
+    private lateinit var readElement: ReadElement
+
+    private var categoryColor: Int = 0
+    private var isSaved: Boolean = false
 
     companion object {
 
@@ -44,7 +53,8 @@ class ReadingActivity : BaseActivity() {
         getIntentData()
         setComponent()
         setTagsRecycler()
-        renderReadElement()
+        renderInitial()
+        setClickListeners()
     }
 
     private fun setComponent() {
@@ -57,20 +67,41 @@ class ReadingActivity : BaseActivity() {
         readElement = intent.getParcelableExtra(EXTRA_READ_ELEMENT)
     }
 
+    private fun renderInitial() {
+        findCategoryColor()
+
+        bottomLayoutLineIv.background = ContextCompat.getDrawable(this, categoryColor)
+        bottomSaveStatusView.background = ContextCompat.getDrawable(this, categoryColor)
+
+        renderReadElement()
+    }
+
+    private fun getShareString(title: String): String {
+        val savedString = getString(R.string.share)
+        return savedString.plus(" ").plus(title).plus(" :")
+    }
+
+    private fun findCategoryColor() {
+        when(readElement.categoryName) {
+            CATEGORY_PARTS -> categoryColor = R.color.parts_color
+            CATEGORY_AMENDMENTS -> categoryColor = R.color.amendment_color
+            CATEGORY_SCHEDULES -> categoryColor = R.color.schedules_color
+            CATEGORY_PREAMBLE -> categoryColor = R.color.preamble_color
+        }
+    }
+
     @Suppress("DEPRECATION")
     private fun renderReadElement() {
-        readElement?.let {
-            titleTv.text = it.title
+        titleTv.text = readElement.title
 
-            if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.N)
-                contentTv.text = Html.fromHtml(it.content)
-            else
-                contentTv.text = Html.fromHtml(it.content, Html.FROM_HTML_MODE_COMPACT)
+        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.N)
+            contentTv.text = Html.fromHtml(readElement.content)
+        else
+            contentTv.text = Html.fromHtml(readElement.content, Html.FROM_HTML_MODE_COMPACT)
 
-            dummyTagsAdapter.submitList(
-                    getTagsFromStrings(it.tags!!, it.categoryName!!)
-            )
-        }
+        dummyTagsAdapter.submitList(
+                getTagsFromStrings(readElement.tags!!, readElement.categoryName!!)
+        )
     }
 
     private fun getTagsFromStrings(stringTags: List<String>, categoryName: String): MutableList<DummyTag> {
@@ -82,6 +113,29 @@ class ReadingActivity : BaseActivity() {
     private fun setTagsRecycler() {
         tagsRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         tagsRv.adapter = dummyTagsAdapter
+    }
+
+    private fun setClickListeners() {
+        bottomSaveStatusView.setOnClickListener {
+            //TODO: save if unsaved, unsave if saved
+            if(isSaved) {
+                saveStatusIv.setImageDrawable(getDrawableFromId(R.drawable.ic_bookmark))
+                saveStatusTv.text = getStringFromId(R.string.save)
+            }
+            else {
+                saveStatusIv.setImageDrawable(getDrawableFromId(R.drawable.ic_bookmark_filled))
+                saveStatusTv.text = getStringFromId(R.string.saved)
+            }
+            isSaved = !isSaved
+        }
+    }
+
+    private fun getStringFromId(id: Int): String {
+        return resources.getString(id)
+    }
+
+    private fun getDrawableFromId(id: Int): Drawable {
+        return ContextCompat.getDrawable(this, id)!!
     }
 
     override fun onBackPressed() {
